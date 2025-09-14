f9085c85032d490b7ab08d3fd8b5a3e2a371993f
Bishwajeet56 <bishwajit.singh5656@gmail.com>
2025-09-14 17:34:56 +0530

    added Q1

---
+++ Q1.py
@@ -0,0 +1,3 @@
+#Q1: Underdamped case
+#A mass of 100 kg is connected with a spring of 10 kN/m. There is a damper of 200 N/s attached to it. 
+#The mass is at rest at .02 m from the equilibrium position.
